270d5dede7bc3a1e0f973b4a7c1fac3630e7ce22
vincentoliveira <vincent@oliveira.fr>
2014-10-01 18:36:55 +0200

    Init

--- ioPOS/LoginViewController.swift
+++ ioPOS/LoginViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var loginTextField : UITextField!
+    @IBOutlet var passwordTextField : UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onLoginClick(sender : AnyObject) {
+        NSLog("Click: %@ / %@", loginTextField.text, passwordTextField.text)
     }
 
 
