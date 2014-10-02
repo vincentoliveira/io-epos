f443633e622a00b169cc249d4522cd0f92bab574
Louis <louis@Vincents-Mac-mini.local>
2014-10-02 15:00:04 +0200

    Adding session controller and deconnexion button

---
+++ ioPOS/TitleViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TitleViewController.swift
+//  ioPOS
+//
+//  Created by Louis on 02/10/14.
+//  Copyright (c) 2014 InnovOrder. All rights reserved.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
+        println("10 rows")
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        println("Row")
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
